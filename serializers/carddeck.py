f03db2d0fd57dd00126a519de3891ef6de83168a
Pierre Carette <pierre.carette@gmail.com>
2014-11-22 00:08:30 +0000

    No Thanks classes

---
+++ serializers/carddeck.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from random import shuffle
+
+class NoThanksCard(object):
+    
+    _card_set = ( '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+             '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+             '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+             '30', '31', '32', '33', '34', '35', '36' )
+    _name = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+        if not self.is_valid():
+            self._name = None
+
+    @property
+    def card_set(self):
+        return self._card_set
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def is_valid(self):
+        return self.name in self.card_set
+
+class NoThanksCardDeck(object):
+
+    _deck = []
+
+    @property
+    def deck(self):
+        return self._deck
+
+    @deck.setter
+    def deck(self, value):
+        self._deck = value
+
+    def __init__(self, card_set=[]):
+        self.deck = list(card_set)
+
+    @property
+    def size(self):
+        return len(self.deck)
+
+    def shuffle(self):
+        shuffle(self.deck)
+        return self
+
+if __name__ == '__main__':
+    deck = NoThanksCardDeck(NoThanksCard().card_set)
+    print deck.deck
+    print deck.size
+    print deck.shuffle().deck
